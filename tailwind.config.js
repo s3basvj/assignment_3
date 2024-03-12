f781b4ab040ceaea0322ea66959859f9503e7cab
SEBASTIAN VARGAS JIMENEZ <sebastian.vargas.jimenez@est.una.ac.cr>
2024-03-11 20:55:21 -0600

    add css step 13

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = { content:["./src/**/*.{ts,tsx}"], theme: { extend: {}, }, plugins: [], }
